17a80a53585700ee6313ad96750545098f842f1b
msoler18 <msoler735@gmail.com>
2018-03-19 21:03:09 -0500

    Add validation for model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   def create  
     product = Product.new(product_params)
     if product.save
-      render json: product, status: 201, location: [:api, product]
+      render json: product, status: 201
     else
       render json: { errors: product.errors }, status: 422  
     end  
